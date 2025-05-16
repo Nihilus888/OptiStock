866588bedc822a507f9da9366761d30b480c9cdf
NAME SURNAME <austonmartin@gmail.com>
2025-05-16 17:29:30 +0800

    added sentry to frontend react

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://19921d0558a6fceeb325244c6341402b@o4509326277869568.ingest.us.sentry.io/4509332081737728",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
