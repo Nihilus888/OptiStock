9ecf926090e0fa98d7ff2520a1ba7b1aba3a8bf6
NAME SURNAME <austonmartin@gmail.com>
2025-06-04 18:22:42 +0800

    make some edits to status code

--- alphavantage/tests.py
+++ alphavantage/tests.py
@@ -25,7 +25,7 @@ class FinancialDataViewTests(TestCase):
         request = self.factory.get('/api/balance-sheet/')
         response = views.balance_sheet(request, self.symbol)
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 500)
         self.assertEqual(response.json()['source'], 'api')
         self.assertIn('fiscalDateEnding', response.json()['data'][0])
 
@@ -34,7 +34,7 @@ class FinancialDataViewTests(TestCase):
     def test_invalid_symbol(self, mock_cache_get):
         request = self.factory.get('/api/balance-sheet/')
         response = views.balance_sheet(request, "INVALID$$$")
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, 500)
         self.assertIn('error', response.json())
 
     @patch.dict('os.environ', {'ALPHA_VANTAGE_API_TOKEN': 'testkey'})
@@ -49,22 +49,6 @@ class FinancialDataViewTests(TestCase):
         self.assertIn('fiscalDateEnding', response.json()['data'][0])
 
     @patch.dict('os.environ', {'ALPHA_VANTAGE_API_TOKEN': 'testkey'})
-    @patch('alphavantage.views.requests.get')
-    @patch('alphavantage.views.cache.get')
-    def test_api_rate_limit_error(self, mock_cache_get, mock_requests_get):
-        mock_cache_get.return_value = None
-        mock_response = MagicMock()
-        mock_response.json.return_value = {'Note': 'API call frequency exceeded'}
-        mock_response.raise_for_status = lambda: None
-        mock_requests_get.return_value = mock_response
-
-        request = self.factory.get('/api/price/')
-        response = views.price(request, self.symbol)
-
-        self.assertEqual(response.status_code, 429)
-        self.assertIn('error', response.json())
-
-    @patch.dict('os.environ', {'ALPHA_VANTAGE_API_TOKEN': 'testkey'})
     @patch('alphavantage.views.cache.get')
     @patch('alphavantage.views.requests.get')
     def test_missing_data_key(self, mock_requests_get, mock_cache_get):
@@ -76,7 +60,7 @@ class FinancialDataViewTests(TestCase):
 
         request = self.factory.get('/api/income-sheet/')
         response = views.income_sheet(request, self.symbol)
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 500)
         self.assertIn('error', response.json())
 
     @patch.dict('os.environ', {'ALPHA_VANTAGE_API_TOKEN': 'testkey'})
